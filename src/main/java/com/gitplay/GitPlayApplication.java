5cc45adc43957ac8c0f8751c057be3b2e2dcce19
nazzaing <41273919+nazzaing@users.noreply.github.com>
2023-03-30 19:18:11 +0900

    first commit

---
+++ src/main/java/com/gitplay/GitPlayApplication.java
@@ -0,0 +1,13 @@
+package com.gitplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPlayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gitplay/GitPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
